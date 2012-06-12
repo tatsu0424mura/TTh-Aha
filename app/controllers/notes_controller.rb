8fc6fe2b6f1bffb18abf404c6c94ea28af205747
Jeff Cohen <jeff@codeacademy.org>
2012-06-12 12:22:01 -0500

    Dynamic editing of notes and theme switching

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -36,6 +36,11 @@ class NotesController < ApplicationController
   # GET /notes/1/edit
   def edit
     @note = Note.find(params[:id])
+    
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # POST /notes
@@ -62,6 +67,7 @@ class NotesController < ApplicationController
 
     respond_to do |format|
       if @note.update_attributes(params[:note])
+        format.js   
         format.html { redirect_to @note, notice: 'Note was successfully updated.' }
         format.json { head :no_content }
       else
